b949fe4556ccd27fbe2faa5d63ba38bf92901fbe
Eduardo Freitas <73096891+Sync-BR@users.noreply.github.com>
2024-10-19 11:41:56 -0300

    Refactor: Estrutura do código. Add Callback.

--- app/src/main/java/com/petrg/meuspets/service/LoginService.java
+++ app/src/main/java/com/petrg/meuspets/service/LoginService.java
@@ -7,8 +7,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.petrg.meuspets.MainActivity;
+import com.petrg.meuspets.callbacks.AuthCallback;
 import com.petrg.meuspets.model.LoginModel;
-import com.petrg.meuspets.util.JsonUtil;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -27,36 +27,40 @@ public class LoginService  {
     private static Context context;
     private static Gson gson = new Gson();
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-    private JsonUtil jsonUtil;
-    public LoginService(Context Context){
+
+    public LoginService(Context Context) {
         cliente = new OkHttpClient();
         context = Context;
     }
 
-    public void autenticar(LoginModel login, Context context){
+    public void autenticar(LoginModel login, Context context, AuthCallback callback) {
         String url = "http://186.247.89.58:8080/api/login/authenticate";
         String json = gson.toJson(login);
-        RequestBody body = RequestBody.create(json,JSON);
+        RequestBody body = RequestBody.create(json, JSON);
         Request request = new Request.Builder().url(url).post(body).build();
         cliente.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 ((MainActivity) context).runOnUiThread(() -> {
-                    Toast.makeText(context, "Erro na solicitação: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
+                    callback.onAuthFailure();
+                    Toast.makeText(context, "Erro na solicitação: ", Toast.LENGTH_SHORT).show();
                 });
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                    ((MainActivity) context).runOnUiThread(() -> {
-                        if(response.isSuccessful()){
-                            Toast.makeText(context, "Sucesso ao autenticar " ,Toast.LENGTH_SHORT).show();
-                        }
-                        Toast.makeText(context, "Login ou senha incorretar " ,Toast.LENGTH_SHORT).show();
-                    });
-
+                ((MainActivity) context).runOnUiThread(() -> {
+                    if (response.isSuccessful()) {
+                        callback.onAuthSuccess();
+                        Toast.makeText(context, "Sucesso ao autenticar ", Toast.LENGTH_SHORT).show();
+                    }
+                    {
+                        callback.onAuthFailure();
+                        Toast.makeText(context, "Login ou senha incorretar ", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
+
         });
 
     }
