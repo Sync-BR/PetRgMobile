00af2b26f238fca132653b695a47d3a8b6e23efc
eduardo freitas <eduardofreitasfr@gmail.com>
2024-10-22 22:33:50 -0300

    Add new class and screen  termo de uso

--- app/src/main/java/com/petrg/meuspets/activity/register/CompleteRegistration.java
+++ app/src/main/java/com/petrg/meuspets/activity/register/CompleteRegistration.java
@@ -1,2 +1,49 @@
-package com.petrg.meuspets.activity.register;public class CompleteRegistration {
+package com.petrg.meuspets.activity.register;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.petrg.meuspets.R;
+import com.petrg.meuspets.implementation.Structure;
+
+public class CompleteRegistration extends AppCompatActivity implements Structure {
+    private CheckBox toAccept;
+    private Button register;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termo_uso);
+    }
+
+    @Override
+    public void initializeUI() {
+        toAccept = findViewById(R.id.text_confirm_termo);
+        register = findViewById(R.id.button_register);
+    }
+
+    @Override
+    public void setupListeners() {
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Ação para completa registro
+            }
+        });
+    }
+
+    @Override
+    public void disableButton() {
+
+    }
+
+    @Override
+    public void enableButton() {
+
+    }
 }
