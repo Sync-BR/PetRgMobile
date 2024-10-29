fe77bed488499aee0adb84fee62d937b58a39b9e
Eduardo <eduardofreitasfr@gmail.com>
2024-10-29 15:42:46 -0300

    refactor

--- app/src/main/java/com/petrg/meuspets/activity/pet/CompletePetRegistration.java
+++ app/src/main/java/com/petrg/meuspets/activity/pet/CompletePetRegistration.java
@@ -1,2 +1,16 @@
-package com.petrg.meuspets.activity.pet;public class CompletePetRegistration {
+package com.petrg.meuspets.activity.pet;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.petrg.meuspets.R;
+
+public class CompletePetRegistration extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_pet_setup2);
+    }
 }
