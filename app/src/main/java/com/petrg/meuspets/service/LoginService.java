b387a09501f297b2f5c490f15071d947cd7582ee
Eduardo <eduardofreitasfr@gmail.com>
2024-10-30 20:23:07 -0300

    add new service

--- app/src/main/java/com/petrg/meuspets/service/LoginService.java
+++ app/src/main/java/com/petrg/meuspets/service/LoginService.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 
 import com.petrg.meuspets.activity.main.MainActivity;
 import com.petrg.meuspets.callbacks.AuthCallback;
+import com.petrg.meuspets.callbacks.usuario.ReceiveUserdataCallBack;
 import com.petrg.meuspets.model.LoginModel;
 
 import okhttp3.Call;
@@ -17,6 +18,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.Callback;
 
 import com.google.gson.Gson;
 
@@ -32,7 +34,27 @@ public class LoginService {
         cliente = new OkHttpClient();
         context = Context;
     }
+    public void getUsuario(String username, final ReceiveUserdataCallBack receiveUserdataCallBack){
+        String url = "http://186.247.89.58:8080/api/login/getUserById/" +username;
+        Request request = new Request.Builder().url(url).build();
+        cliente.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                receiveUserdataCallBack.onError(e);
+            }
 
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseData = response.body().string();
+                    LoginModel loginModel = gson.fromJson(responseData, LoginModel.class);
+                    receiveUserdataCallBack.onSuccess(loginModel);
+                } else {
+                    receiveUserdataCallBack.onError(new IOException("Unexpected code: " + response));
+                }
+            }
+        });
+    }
     public void autenticar(LoginModel login, Context context, AuthCallback callback) {
         String url = "http://186.247.89.58:8080/api/login/authenticate";
         String json = gson.toJson(login);
