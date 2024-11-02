cf771ef2a4a8df3ea7b248a1b43035d52470caf8
Eduardo <eduardofreitasfr@gmail.com>
2024-11-02 20:42:25 -0300

    Added pet return

--- app/src/main/java/com/petrg/meuspets/activity/user/UserActivity.java
+++ app/src/main/java/com/petrg/meuspets/activity/user/UserActivity.java
@@ -2,34 +2,35 @@ package com.petrg.meuspets.activity.user;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.petrg.meuspets.R;
 import com.petrg.meuspets.activity.pet.RegisterPetActivity;
+import com.petrg.meuspets.adapter.PetAdapter;
 import com.petrg.meuspets.callbacks.usuario.PetCallBack;
 import com.petrg.meuspets.implementation.Structure;
 import com.petrg.meuspets.model.LoginModel;
 import com.petrg.meuspets.model.PetModel;
 import com.petrg.meuspets.service.register.ValidationService;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class UserActivity extends AppCompatActivity implements Structure {
     private Toolbar toolbar;
+    private RecyclerView petList;
     private AppCompatImageButton addNewPet;
     private LoginModel loginModel;
-    private ValidationService validarPets = new ValidationService(this);
-
+    private ValidationService validarPets;
+    private PetAdapter petAdapter;
+    private List<PetModel> listPet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,14 +39,20 @@ public class UserActivity extends AppCompatActivity implements Structure {
         setupListeners();
         validatePets();
         setSupportActionBar(toolbar);
+        listPet = new ArrayList<>();
+        petAdapter = new PetAdapter(listPet);
+        petList.setLayoutManager(new LinearLayoutManager(this));
+        petList.setAdapter(petAdapter);
     }
 
     @Override
     public void initializeUI() {
         toolbar = findViewById(R.id.toolbar);
-        Intent intent = getIntent();
+        petList = findViewById(R.id.recyclerViewPets);
         addNewPet = findViewById(R.id.add_pet_button);
+        Intent intent = getIntent();
         loginModel = (LoginModel) intent.getSerializableExtra("usuario");
+        validarPets = new ValidationService(this);
     }
 
     @Override
@@ -79,7 +86,10 @@ public class UserActivity extends AppCompatActivity implements Structure {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.menu_perfil) {
-            Toast.makeText(this, "Abrindo Perfil", Toast.LENGTH_SHORT).show();
+            //User profile screen
+            Intent screenUserProfile = new Intent(this, UserPerfilActivity.class);
+            screenUserProfile.putExtra("usuario", loginModel);
+            startActivity(screenUserProfile);
             return true;
         } else if (id == R.id.menu_sair) {
             Toast.makeText(this, "Saindo", Toast.LENGTH_SHORT).show();
@@ -93,16 +103,22 @@ public class UserActivity extends AppCompatActivity implements Structure {
         validarPets.getPetUser(loginModel.getUsuarioModel().getId(), new PetCallBack() {
             @Override
             public void onSuccess(List<PetModel>[] pet) {
-                List<PetModel> listPets = new ArrayList<>();
                 for (List<PetModel> pets : pet) {
-                    listPets.addAll(pets);
+                    listPet.addAll(pets);
                 }
                 addNewPet.setVisibility(View.INVISIBLE);
+                petList.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void empty() {
                 addNewPet.setVisibility(View.VISIBLE);
+                ViewGroup.LayoutParams params = petList.getLayoutParams();
+                int heightInPixels = (int) (300 * getResources().getDisplayMetrics().density);
+                params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                params.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+                petList.setLayoutParams(params);
+                petList.setVisibility(View.INVISIBLE);
             }
 
             @Override
